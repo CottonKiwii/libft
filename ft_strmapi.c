f196875d70baf6e8e66bda746d55857bb552f1fd
Julia Wolfram <julia.wolfram@gmx.at>
2024-04-12 11:28:08 +0200

    did striteri and strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,9 +6,27 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 11:56:05 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/04/11 11:56:53 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/04/12 11:12:24 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{}
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	str = (char *)ft_calloc((ft_strlen((char *)s) + 1), sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	return (str);
+}
