b7d55243c6af4dbe3e0940562b68897f517f7386
Julia Wolfram <julia.wolfram@gmx.at>
2024-04-12 11:48:51 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -6,27 +6,43 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 17:08:05 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/04/10 19:33:58 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/04/12 11:48:42 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_setarray(char **arr, int start, int len)
+static int	ft_wordcount(char const *s, char c)
 {
+	int	word;
 	int	i;
 
+	word = 0;
 	i = 0;
+	if (s[i] != c)
+	{
+		word++;
+		i++;
+	}
+	while (s[i])
+	{
+		if (s[i] == c && s[i + 1] != c)
+			word++;
+		i++;
+	}
+	return (word);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
 	int		start;
-	int		len;
+	int		word;
 	int		i;
 
 	i = 0;
 	if (!s)
 		return (NULL);
+	word = ft_wordcount(s, c);
+	return (arr);
 }
